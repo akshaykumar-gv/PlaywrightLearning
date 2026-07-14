02f81aba35898893f36c199118c263b9d0aa2443
Akshay GV <akshaykumar.gv@gmail.com>
2026-07-14 08:56:06 +0530

    Add JavaScript basics and identifier rules examples

---
+++ Chapter_03_Identifiers/03_Identifiers_Rules.js
@@ -0,0 +1,18 @@
+var a = 10;
+console.log(a);
+
+var $ = 10;
+var _a = 23;
+var pp = 34;
+
+var ab123 = 23;
+// var 45 = 34;
+var _ = 10;
+
+var Name = "Akshay";
+var name = "Akshay";
+
+// var akshay kumar = "hello";
+var akshay_kumar = "hello";
+var akshay$kumar = "hello";
+var pramodu1232 = "hello";
